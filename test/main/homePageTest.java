b3d2c17fbb2a5cb1be1d396f17ef18eca78cb964
Makii117 <MakiOnPc@gmail.com>
2022-01-15 01:39:52 +0100

    Darkmode test fix, added test for download button

--- test/main/homePageTest.java
+++ test/main/homePageTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,11 +41,41 @@ public class homePageTest {
         Thread.sleep(1000);
 
         WebElement darkmodeButton = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[3]/div/div[1]/ul/li[6]/div/label"));
+        //check if darkmode is currently on or off
+        if(darkmodeButton.getAttribute("class").equals("input-switch input-switch is-checked")){
 
-        darkmodeButton.click();
-        assertEquals("input-switch input-switch is-checked",darkmodeButton.getAttribute("class"));
-        Thread.sleep(1000);
+            darkmodeButton.click();
+            assertEquals("input-switch input-switch",darkmodeButton.getAttribute("class"));
+            Thread.sleep(5000);
+        }else{
+            darkmodeButton.click();
+            assertEquals("input-switch input-switch is-checked",darkmodeButton.getAttribute("class"));
+            Thread.sleep(5000);
+        }
+
+
     }
+    @Test
+    void downloadButton(){
+        try{
+            Thread.sleep(1000);
+            WebElement downloadBtn = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div/div[2]/div[1]/a"));
+            downloadBtn.click();
+            Thread.sleep(1000);
+
+            ArrayList<String> tabs2 = new ArrayList<String> (webDriver.getWindowHandles());
+            webDriver.switchTo().window(tabs2.get(0));
+            webDriver.close();
+            webDriver.switchTo().window(tabs2.get(1));
+            Thread.sleep(1000);
+            assertEquals("https://www.deezer.com/en/download",webDriver.getCurrentUrl());
+            Thread.sleep(1000);
+        }catch (ElementNotVisibleException | InterruptedException e){
+            System.out.println("Download button not visible");
+            System.out.println(e);
+        }
+    }
+
 
 }
 
